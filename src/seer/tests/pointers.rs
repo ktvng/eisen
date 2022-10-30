18dc7311e4f64d31988f646527edc7209bad6602
ktvng <35818433+ktvng@users.noreply.github.com>
2022-10-29 18:13:14 -0700

    add tests

---
+++ src/seer/tests/pointers.rs
@@ -0,0 +1,24 @@
+fn max(x: int var, y: int var) -> r: int var {
+    if (x > y) {
+        r = x
+    }
+    else {
+        r = y
+        y <- 0
+    }
+}
+
+fn letwhy(x: int) -> r: int {
+    r = x
+}
+
+fn main() {
+    let a, b = 15, 9
+    var x = max(a, b)
+    print("%i ", x)
+    a = 4
+    print("%i ", x)
+
+    x = max(a, b)
+    print("%i", x)
+}

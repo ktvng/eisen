732a19ca4cbe1d7a911eff0980996aca33474d0d
ktvng <35818433+ktvng@users.noreply.github.com>
2022-12-23 01:28:46 -0500

    refactor

--- src/eisen/validation/callunwrapper.py
+++ src/eisen/validation/callunwrapper.py
@@ -8,64 +8,59 @@ from eisen.validation.nodetypes import Nodes
 
 class CallUnwrapper():
     @classmethod
-    # to unravel a (raw_call ...) is to perform the following action
-    #   
-    #   (raw_call (ref name) (fn name) (params ...)) ->
-    #   (call (fn name) (params (ref name) ...)) 
     def _unravel(cls, params: State):
+        """perform the following operation to "unravel" an asl:
+            (raw_call (ref obj) (fn funcName) (params ...)))
+        becomes the asl
+            (call (fn funcName) (params (ref obj) ...))
+        """
         node = Nodes.RawCall(params)
-        ref_asl = node.get_ref_asl()
-        fn_asl = node.get_fn_identifying_asl()
         params_asl = node.get_params_asl()
 
         # modify the contents of the params asl to include the reference as the first
         # argument.
-        params_asl[:] = [ref_asl, *params_asl]
+        params_asl[:] = [node.get_ref_asl(), *params_asl]
 
         # update the (raw_call ...) asl with the new lst contents
         params.asl.update(
             type="call",
-            lst=[fn_asl, params_asl])
+            lst=[node.get_fn_asl(), params_asl])
 
     @classmethod
-    def _construct_standard_call(cls, params: State):
-        node = Nodes.RawCall(params)
-        ref_asl = node.get_ref_asl()
-        fn_asl = node.get_fn_identifying_asl()
-        print(fn_asl)
-        params_asl = node.get_params_asl()
-
-        scope_asl = CLRList(
+    def _create_new_scope_asl(cls, node: Nodes.RawCall) -> CLRList:
+        """create a scope resolution asl like (. name attribute)"""
+        return CLRList(
             type=".",
-            lst=[ref_asl, fn_asl.first()],
-            line_number=params.asl.line_number,
+            lst=[node.get_ref_asl(), node.get_fn_asl().first()],
+            line_number=node.line_number(),
             data=NodeData())
 
-        new_fn_asl = CLRList(
+    @classmethod
+    def _create_new_fn_asl(cls, resolves_to_fn: CLRList) -> CLRList:
+        """create a new (fn ...) asl for something that resolves_to_fn"""
+        return CLRList(
             type="fn",
-            lst=[scope_asl],
-            line_number=params.asl.line_number,
-            data=NodeData())
+            lst=[resolves_to_fn],
+            line_number=resolves_to_fn.line_number,
+            date=NodeData())
 
+    @classmethod
+    def _convert_to_normal_call(cls, params: State):
+        """convert, inplace an asl of form
+            (raw_call (ref name) (fn attr) (params ...)
+        to an asl of form
+            (call (fn (. name attr)) (params ...)))
+        """
+        node = Nodes.RawCall(params)
+        scope_asl = cls._create_new_scope_asl(node)
+        new_fn_asl = cls._create_new_fn_asl(scope_asl)
         params.asl.update(
             type="call",
-            lst=[new_fn_asl, params_asl])
+            lst=[new_fn_asl, node.get_params_asl()])
 
     @classmethod
     def process(cls, params: State):
-        if cls._should_unravel(params):
+        if Nodes.RawCall(params).calls_member_function():
+            cls._convert_to_normal_call(params)
+        else:
             cls._unravel(params)
-            return
-        cls._construct_standard_call(params)
-
-
-    # case is (raw_call (ref x) (fn run) (params )))))
-    # note that (ref x) could also be an expression
-    @classmethod
-    def _should_unravel(cls, params: State) -> bool:
-        typeclass = params.but_with(asl=params.asl.first()).get_returned_typeclass()
-        secondary_name = params.asl.second().first().value
-        if typeclass.is_struct() and typeclass.has_member_attribute_with_name(secondary_name):
-            return False
-
-        return True

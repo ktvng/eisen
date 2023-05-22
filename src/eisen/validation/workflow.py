83f36e8150d4f901f760d0203c93293e670ec294
ktvng <35818433+ktvng@users.noreply.github.com>
2023-05-22 05:35:46 -0700

    refactoring

--- src/eisen/validation/workflow.py
+++ src/eisen/validation/workflow.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
 import time
+from dataclasses import dataclass
 
+from alpaca.utils import Visitor
 from eisen.common.exceptionshandler import ExceptionsHandler
 from eisen.validation.modulevisitor import ModuleVisitor
 from eisen.validation.typechecker import TypeChecker
@@ -23,13 +25,14 @@ from eisen.state.basestate import BaseState as State
 # for use as instances)
 # They are implemented the same.
 
+# class used to debug
 class PrintAsl():
     def run(self, state: State):
         print(state.asl)
         return state
 
 class Workflow():
-    steps = [
+    steps: list[Visitor] = [
         # initialize the .data attribute for all asls with empty NodeData instances
         Initializer,
 
@@ -75,52 +78,76 @@ class Workflow():
         # note: def, create, fn, ref, ilet, :: need instances!!
     ]
 
-    @classmethod
-    def execute(cls, state: State, steps=None):
-        if steps is None:
-            steps = cls.steps
+    @staticmethod
+    def _choose_steps(supplied_steps: list[Visitor]=None):
+        if supplied_steps:
+            return supplied_steps
+        return Workflow.steps
 
-        for step in steps:
-            state = step().run(state)
+    @staticmethod
+    def _run_step_with_state(step: Visitor, state: State) -> State:
+        return step().run(state)
 
+    @staticmethod
+    def execute(state: State, steps: list[Visitor]=None) -> tuple[bool, State]:
+        for step in Workflow._choose_steps(steps):
+            state = Workflow._run_step_with_state(step, state)
             ExceptionsHandler().apply(state)
-            if cls.should_stop_execution(state):
+            if Workflow.should_stop_execution(state):
                 return False, state
-
         return True, state
 
-    @classmethod
-    def execute_with_benchmarks(cls, state: State, steps=None):
-        if steps is None:
-            steps = cls.steps
-        perf = []
-        def step_decorator(step):
-            class decorated_step():
-                def run(self, state: State):
-                    start = time.perf_counter_ns()
-                    state = step().run(state)
-                    end = time.perf_counter_ns()
-                    perf.append((step.__name__, round((end-start)/1000000, 5)))
-                    return state
+    @staticmethod
+    def _instrument_step_with_telemetry(step_times: list[int], step: Visitor) -> Visitor:
+        class decorated_step():
+            def run(self, state: State):
+                start = time.perf_counter_ns()
+                state = Workflow._run_step_with_state(step, state)
+                end = time.perf_counter_ns()
+                decorated_step.add_metric(start, end)
+                return state
 
-            return decorated_step
+            @staticmethod
+            def add_metric(start: int, end: int):
+                step_times.append(PerformanceMetric(
+                    step_name=step.__name__,
+                    step_time=round((end-start)/1000000, 5)))
 
-        decorated_workflow = [step_decorator(step) for step in steps]
-        result, state = cls.execute(state, steps=decorated_workflow)
-        if not result:
-            print("Failed to run...")
-        cls.pretty_print_perf(perf)
-        return state
+        return decorated_step
 
-    @classmethod
-    def pretty_print_perf(cls, perf: list[tuple[str, int]]):
-        longest_name_size = max(len(x[0]) for x in perf)
+    @staticmethod
+    def execute_with_benchmarks(state: State, steps: list[Visitor]=None) -> tuple[bool, State]:
+        metrics: list[PerformanceMetric] = []
+        steps_with_telemetry = [Workflow._instrument_step_with_telemetry(metrics, step)
+            for step in Workflow._choose_steps(steps)]
+
+        result, state = Workflow.execute(state, steps=steps_with_telemetry)
+        PerformanceMetric.print_performance_metrics(metrics)
+        return result, state
+
+    @staticmethod
+    def should_stop_execution(state: State) -> bool:
+        return len(state.exceptions) > 0
+
+
+@dataclass
+class PerformanceMetric():
+    step_name: str
+    step_time: int
+
+    @staticmethod
+    def _print_performance_metric_line(metric: PerformanceMetric, block_size: int):
+        print(" "*(block_size - len(metric.step_name)),
+            metric.step_name, " ",
+            metric.step_time)
+
+    @staticmethod
+    def print_performance_metrics(metrics: list[PerformanceMetric]):
+        longest_name_size = max(len(metric.step_name) for metric in metrics)
         block_size = longest_name_size + 4
-        for name, val in perf:
-            print(" "*(block_size - len(name)), name, " ", val)
+        for metric in metrics:
+            PerformanceMetric._print_performance_metric_line(metric, block_size)
 
-        print(" "*(block_size-len("Total")), "Total", " ", round(sum(x[1] for x in perf), 5))
-
-    @classmethod
-    def should_stop_execution(cls, state: State):
-        return state.exceptions
+        print(" "*(block_size-len("Total")),
+              "Total", " ",
+              round(sum(metric.step_time for metric in metrics), 5))

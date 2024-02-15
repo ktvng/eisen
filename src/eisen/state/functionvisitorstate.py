c5d87a37005342233eff5401c006b94c22d647bd
ktvng <35818433+ktvng@users.noreply.github.com>
2024-02-14 17:37:31 -0800

    state changes

--- src/eisen/state/functionvisitorstate.py
+++ src/eisen/state/functionvisitorstate.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
+from dataclasses import dataclass
 
 from alpaca.concepts import Module, Context
 from alpaca.clr import AST
 
 from eisen.common.eiseninstance import Instance
+from eisen.common.traits import TraitImplementation, TraitImplDetailsForFunctionVisitor
 from eisen.state.basestate import BaseState
-
+from eisen.typecheck.typeparser import TypeParser
 
 class FunctionVisitorState(BaseState):
     """
@@ -20,20 +22,31 @@ class FunctionVisitorState(BaseState):
             ast: AST = None,
             context: Context = None,
             mod: Module = None,
-            struct_name: str = None
+            struct_name: str = None,
+            trait_impl_details: TraitImplDetailsForFunctionVisitor = None
             ) -> FunctionVisitorState:
 
         return self._but_with(
             ast=ast,
             context=context,
             mod=mod,
-            struct_name=struct_name,)
+            struct_name=struct_name,
+            trait_impl_details=trait_impl_details)
 
 
     @staticmethod
     def create_from_basestate(state: BaseState):
-        return FunctionVisitorState(**state._get(), struct_name="")
+        return FunctionVisitorState(**state._get(),
+                                    type_parser=TypeParser(),
+                                    struct_name="",
+                                    trait_impl_details=None)
 
+    def get_trait_impl_details(self) -> TraitImplDetailsForFunctionVisitor | None:
+        """
+        Get the details about the trait/implementing struct, which is not None for any context
+        that occurs inside a (trait_def ...) AST
+        """
+        return self.trait_impl_details
 
     def get_struct_name(self) -> str:
         """
@@ -54,3 +67,12 @@ class FunctionVisitorState(BaseState):
         :type instance: EisenInstance
         """
         self.get_enclosing_module().add_function_instance(instance)
+
+    def get_type_parser(self) -> TypeParser:
+        return self.type_parser
+
+    def add_trait_implementation(self, impl: TraitImplementation):
+        self.get_enclosing_module().add_obj("trait_implementations", impl.get_key_for_this(), impl)
+
+    def this_is_trait_implementation(self) -> bool:
+        return self.trait_impl_details is not None
